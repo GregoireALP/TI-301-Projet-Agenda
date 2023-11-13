6d5562b4f839946db4fba8299d52004604f2966a
GregoireALP <gregoire.alperovitch@gmail.com>
2023-11-13 18:09:09 +0100

    Lists

--- src/fichier.c
+++ src/fichier.c
@@ -12,11 +12,9 @@ t_d_cell *create_cell(int value, int level) {
 
     cell->value = value;
     cell->level = level;
-
-    // Set all next pointers to NULL
     cell->next = (t_d_cell**) malloc(sizeof(t_d_cell*) * level);
-    for(int i = 0; i < level; i++) {
 
+    for(int i = 0; i < level; i++) {
         cell->next[i] = NULL;
     }
 
@@ -25,12 +23,11 @@ t_d_cell *create_cell(int value, int level) {
 
 void display_cell(t_d_cell* cell) {
 
-    // For each level in the cell
     for(int i = 0; i < cell->level; i++) {
-
         printf("[ %d|@ ]-->\n", cell->value);
-
     }
+
+    return;
 }
 
 /**********************************************************************************/
@@ -40,34 +37,36 @@ void display_cell(t_d_cell* cell) {
 
 t_d_list *create_list(int max_level) {
 
-    t_d_list *list = (t_d_list*) malloc(sizeof(t_d_list));
-    list->max_level = max_level;
+        t_d_list *list = (t_d_list*) malloc(sizeof(t_d_list));
 
-    // Set all heads pointers to NULL
-    list->heads = (t_d_cell**) malloc(sizeof(t_d_cell*) * max_level);
-    for(int i = 0; i < list->max_level; i++) {
-        list->heads[i] = NULL;
-    }
+        list->max_level = max_level;
+        list->heads = (t_d_cell**) malloc(sizeof(t_d_cell*) * max_level);
 
-    return list;
+        for(int i = 0; i < max_level; i++) {
+
+            list->heads[i] = NULL;
+
+        }
+
+        return list;
 }
 
 void display_list(t_d_list* list) {
-    
-    // Pour chaque niveaux
+
     for(int i = 0; i < list->max_level; i++) {
 
-        printf("[list head_%d] @-]-->", i);
-        t_d_cell * tmp = list->heads[i];
+        printf("[head_%d -@]-->", i);
+        t_d_cell *tmp = list->heads[i];
 
-        if(tmp != NULL) {
-            while(tmp->next != NULL) {
-                printf("[ %d|@ ]-->", tmp->value);
-                tmp = tmp->next;
-            }
+        while(tmp != NULL) {
+
+            printf("[ %d|@ ]-->", tmp->value);
+            tmp = tmp->next[i];
+
         }
 
         printf("NULL\n");
+
     }
 
 }
@@ -76,6 +75,49 @@ void display_list(t_d_list* list) {
     /*                                 ACTIONS                                        */
     /**********************************************************************************/
 
-void insert_cell(t_d_cell* cell, t_d_list* list) {
 
+void insert_cell(int value , int level, t_d_list* list) {
+
+    t_d_cell *cell = create_cell(value, level);
+
+    // For each level of the list
+    for(int i = 0; i < level; i++) {
+
+        // If the list is empty
+        if(list->heads[i] == NULL) {
+
+            // Insert the cell at the beginning of the list
+            list->heads[i] = cell;
+
+        } else {
+
+            // If the cell have to be insert at the beginning of the list
+            if(list->heads[i]->value > value) {
+
+                // Insert the cell at the beginning of the list
+                cell->next[i] = list->heads[i];
+                list->heads[i] = cell;
+
+            } else {
+
+                // If the cell have to be insert in the middle of the list
+                t_d_cell *tmp = list->heads[i];
+
+                while(tmp->next[i] != NULL && tmp->next[i]->value < value) {
+
+                    tmp = tmp->next[i];
+
+                }
+
+                // Insert the cell in the middle of the list
+                cell->next[i] = tmp->next[i];
+                tmp->next[i] = cell;
+
+            }
+
+        }
+
+    }
+
+    return;
 }
--- src/main.c
+++ src/main.c
@@ -20,7 +20,14 @@ int main() {
     // Empty List Demo
     int myEmptyListLevel = 5;
     t_d_list* myEmptyList = create_list(myEmptyListLevel);
-    //display_list(myEmptyList);
+   //display_list(myEmptyList);
+
+    // Insert Cell Demo
+    insert_cell(3, 3, myEmptyList);
+    insert_cell(5, 2, myEmptyList);
+    insert_cell(4, 3, myEmptyList);
+    insert_cell(7, 4, myEmptyList);
+    display_list(myEmptyList);
 
     return 0;
 }
